c86b0efd073a11f72cb0ac14f7ba4eb80b227ca4
風間和彦 <kazamak@gmail.com>
2020-10-17 15:15:30 +0900

    プロジェクト ファイルを追加します。

---
+++ Timer/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Timer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TextBox_ElapsedTime = new System.Windows.Forms.TextBox();
+            this.button_A = new System.Windows.Forms.Button();
+            this.button_B = new System.Windows.Forms.Button();
+            this.Color_panel = new System.Windows.Forms.Panel();
+            this.button_Z = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Time_Records = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button_C = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // TextBox_ElapsedTime
+            // 
+            this.TextBox_ElapsedTime.Cursor = System.Windows.Forms.Cursors.No;
+            this.TextBox_ElapsedTime.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TextBox_ElapsedTime.Location = new System.Drawing.Point(23, 64);
+            this.TextBox_ElapsedTime.Name = "TextBox_ElapsedTime";
+            this.TextBox_ElapsedTime.Size = new System.Drawing.Size(191, 39);
+            this.TextBox_ElapsedTime.TabIndex = 0;
+            this.TextBox_ElapsedTime.Text = "00:00";
+            this.TextBox_ElapsedTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button_A
+            // 
+            this.button_A.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button_A.Location = new System.Drawing.Point(23, 35);
+            this.button_A.Name = "button_A";
+            this.button_A.Size = new System.Drawing.Size(91, 23);
+            this.button_A.TabIndex = 1;
+            this.button_A.Text = "A";
+            this.button_A.UseVisualStyleBackColor = true;
+            this.button_A.Click += new System.EventHandler(this.button_A_Click);
+            // 
+            // button_B
+            // 
+            this.button_B.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button_B.Location = new System.Drawing.Point(120, 35);
+            this.button_B.Name = "button_B";
+            this.button_B.Size = new System.Drawing.Size(94, 24);
+            this.button_B.TabIndex = 2;
+            this.button_B.Text = "B";
+            this.button_B.UseVisualStyleBackColor = true;
+            this.button_B.Click += new System.EventHandler(this.button_B_Click);
+            // 
+            // Color_panel
+            // 
+            this.Color_panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Color_panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Color_panel.Location = new System.Drawing.Point(220, 9);
+            this.Color_panel.Name = "Color_panel";
+            this.Color_panel.Size = new System.Drawing.Size(524, 471);
+            this.Color_panel.TabIndex = 4;
+            // 
+            // button_Z
+            // 
+            this.button_Z.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_Z.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button_Z.Location = new System.Drawing.Point(23, 443);
+            this.button_Z.Name = "button_Z";
+            this.button_Z.Size = new System.Drawing.Size(191, 23);
+            this.button_Z.TabIndex = 5;
+            this.button_Z.Text = "Z";
+            this.button_Z.UseVisualStyleBackColor = true;
+            this.button_Z.Click += new System.EventHandler(this.button_Z_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 9);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Time_Records
+            // 
+            this.Time_Records.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Time_Records.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Time_Records.Location = new System.Drawing.Point(23, 109);
+            this.Time_Records.Multiline = true;
+            this.Time_Records.Name = "Time_Records";
+            this.Time_Records.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Time_Records.Size = new System.Drawing.Size(191, 303);
+            this.Time_Records.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(154, 469);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Version";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 424);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "COM Port";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(82, 420);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(53, 20);
+            this.comboBox2.TabIndex = 10;
+            // 
+            // button_C
+            // 
+            this.button_C.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_C.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button_C.Location = new System.Drawing.Point(141, 418);
+            this.button_C.Name = "button_C";
+            this.button_C.Size = new System.Drawing.Size(73, 23);
+            this.button_C.TabIndex = 11;
+            this.button_C.Text = "C";
+            this.button_C.UseVisualStyleBackColor = true;
+            this.button_C.Click += new System.EventHandler(this.button_C_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 492);
+            this.Controls.Add(this.button_C);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Time_Records);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button_Z);
+            this.Controls.Add(this.Color_panel);
+            this.Controls.Add(this.button_B);
+            this.Controls.Add(this.button_A);
+            this.Controls.Add(this.TextBox_ElapsedTime);
+            this.Name = "MainForm";
+            this.Text = "TM_Timer_main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox_ElapsedTime;
+        private System.Windows.Forms.Button button_A;
+        private System.Windows.Forms.Button button_B;
+        private System.Windows.Forms.Panel Color_panel;
+        private System.Windows.Forms.Button button_Z;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox Time_Records;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button_C;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ Timer/MainForm.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.IO.Ports;
+
+namespace Timer
+{
+    public partial class MainForm : Form
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+        string currentTime = string.Empty;
+        int speech_num = 0;
+        Boolean com_connected = false;
+
+        struct _speechTime
+        {
+            public string name;
+            public TimeSpan time_Green;
+            public TimeSpan time_Yellow;
+            public TimeSpan time_Red;
+            public TimeSpan time_End;
+
+            public _speechTime(string name, TimeSpan time_Green, TimeSpan time_Yellow, TimeSpan time_Red, TimeSpan time_End)
+            {
+                this.name = name;
+                this.time_Green = time_Green;
+                this.time_Yellow = time_Yellow;
+                this.time_Red = time_Red;
+                this.time_End = time_End;
+            }
+        };
+
+        _speechTime[] SpeechTime =
+        {
+            new _speechTime("テーブルトピックス (1-2分)",
+                           TimeSpan.FromMinutes(1.0),
+                           TimeSpan.FromMinutes(1.5),
+                           TimeSpan.FromMinutes(2.0),
+                           TimeSpan.FromMinutes(2.5)),
+            new _speechTime("準備スピーチ（4-6分)",
+                           TimeSpan.FromMinutes(4.0),
+                           TimeSpan.FromMinutes(5.0),
+                           TimeSpan.FromMinutes(6.0),
+                           TimeSpan.FromMinutes(6.5)),
+            new _speechTime("準備スピーチ (5-7分)",
+                           TimeSpan.FromMinutes(5.0),
+                           TimeSpan.FromMinutes(6.0),
+                           TimeSpan.FromMinutes(7.0),
+                           TimeSpan.FromMinutes(7.5)),
+            new _speechTime("準備スピーチ (8-10分)",
+                           TimeSpan.FromMinutes(8.0),
+                           TimeSpan.FromMinutes(9.0),
+                           TimeSpan.FromMinutes(10.0),
+                           TimeSpan.FromMinutes(10.5)),
+            new _speechTime("準備スピーチ (10-12分)",
+                           TimeSpan.FromMinutes(10.0),
+                           TimeSpan.FromMinutes(11.0),
+                           TimeSpan.FromMinutes(12.0),
+                           TimeSpan.FromMinutes(12.5)),
+            new _speechTime("準備スピーチ (12-15分)",
+                           TimeSpan.FromMinutes(12.0),
+                           TimeSpan.FromMinutes(13.5),
+                           TimeSpan.FromMinutes(15.0),
+                           TimeSpan.FromMinutes(15.5)),
+            new _speechTime("論評 (2-3分)",
+                           TimeSpan.FromMinutes(2.0),
+                           TimeSpan.FromMinutes(2.5),
+                           TimeSpan.FromMinutes(3.0),
+                           TimeSpan.FromMinutes(3.5)),
+            new _speechTime("**テストモード** (3秒毎)",
+                           TimeSpan.FromSeconds(3.0),
+                           TimeSpan.FromSeconds(6.0),
+                           TimeSpan.FromSeconds(9.0),
+                           TimeSpan.FromSeconds(12.0))
+        };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Color_panel.BackColor = System.Drawing.Color.FromArgb(0x00, 0x00, 0x00);
+
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
+
+            this.Text = "TM_Timer";
+            button_A.Text = "開始";
+            button_B.Text = "クリア";
+            button_Z.Text = "終了";
+
+            this.label1.Text = "Ver.0.2.6";
+            this.ActiveControl = this.comboBox1;
+
+            button_C.Text = "接続";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i< SpeechTime.Length ; i++)
+            {
+                comboBox1.Items.Add(SpeechTime[i].name.ToString());
+            }
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBox2.Items.Add(port);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            speech_num = comboBox1.SelectedIndex;
+
+            Time_Records.AppendText(SpeechTime[speech_num].name.ToString());
+            Time_Records.AppendText("\r\n");
+            button_A.Select();
+        }
+
+        private void button_A_Click(object sender, EventArgs e)
+        {
+            if(sw.IsRunning)
+            {
+                sw.Stop();
+                button_A.Text = "開始";
+            }
+            else
+            {
+                sw.Start();
+                dt.Start();
+                button_A.Text = "停止";
+            }
+        }
+
+        private void button_B_Click(object sender, EventArgs e)
+        {
+            TimeSpan ts = sw.Elapsed;
+
+            currentTime = String.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+            Time_Records.AppendText( currentTime );
+            if( ts > SpeechTime[speech_num].time_End )
+            {
+                Time_Records.AppendText("(++)");
+            } else if(ts < SpeechTime[speech_num].time_Green)
+            {
+                Time_Records.AppendText("(--)");
+            }
+            Time_Records.AppendText("\r\n");
+
+            sw.Stop();
+            sw.Reset();
+            dt.Stop();
+
+            this.Color_panel.BackColor = System.Drawing.Color.FromArgb(0x00, 0x00, 0x00);
+            if ( com_connected)
+            {
+                serialPort1.Write("B");
+            }
+            TextBox_ElapsedTime.Text = "00:00";
+            button_A.Text = "開始";
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                TimeSpan ts = sw.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}", ts.Minutes, ts.Seconds);
+                TextBox_ElapsedTime.Text = currentTime;
+
+                if (ts.Minutes == SpeechTime[speech_num].time_Green.Minutes && 
+                    ts.Seconds == SpeechTime[speech_num].time_Green.Seconds)
+                {
+                    this.Color_panel.BackColor = System.Drawing.Color.FromArgb(0x00, 0x80, 0x00);
+                    if ( com_connected )
+                    {
+                        serialPort1.Write("G");
+                    }
+                }
+                else if (ts.Minutes == SpeechTime[speech_num].time_Yellow.Minutes && 
+                         ts.Seconds == SpeechTime[speech_num].time_Yellow.Seconds)
+                {
+                    this.Color_panel.BackColor = System.Drawing.Color.FromArgb(0xFF, 0xFF, 0x00);
+                    if ( com_connected)
+                    {
+                        serialPort1.Write("Y");
+                    }
+                }
+                else if (ts.Minutes == SpeechTime[speech_num].time_Red.Minutes && 
+                         ts.Seconds == SpeechTime[speech_num].time_Red.Seconds)
+                {
+                    this.Color_panel.BackColor = System.Drawing.Color.FromArgb(0xFF, 0x00, 0x00);
+                    if ( com_connected )
+                    {
+                        serialPort1.Write("R");
+                    }
+                }
+                else if (ts.Minutes == SpeechTime[speech_num].time_End.Minutes && 
+                         ts.Seconds == SpeechTime[speech_num].time_End.Seconds)
+                {
+                    this.Color_panel.BackColor = System.Drawing.Color.FromArgb(0x00, 0x00, 0x00);
+                    if ( com_connected )
+                    {
+                        serialPort1.Write("B");
+                    }
+                }
+            }
+        }
+
+        private void button_Z_Click(object sender, EventArgs e)
+        {
+            sw.Stop();
+            dt.Stop();
+            this.Close();
+        }
+
+        private void button_C_Click(object sender, EventArgs e)
+        {
+            if (comboBox2.Text != "")
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox2.SelectedItem.ToString();
+                    serialPort1.Open();
+
+                    serialPort1.Write("S");
+                    System.Threading.Thread.Sleep(200);
+                    string data = serialPort1.ReadExisting();
+                    if (!string.IsNullOrEmpty(data) && data == "O")
+                    {
+                        button_C.Enabled = false;
+                        comboBox2.Enabled = false;
+                        com_connected = true;
+                        this.ActiveControl = this.comboBox1;
+                    }
+                    else
+                    {
+                        serialPort1.Close();
+                        MessageBox.Show("接続できませんでした。");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("COM portを選択して下さい。");
+            }
+        }
+    }
+}
